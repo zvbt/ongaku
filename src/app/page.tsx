99c8ffe5d6369f1e847393fc7a47dae7ef745ee3
zvbt <cyadine@skiff.com>
2024-04-17 02:55:41 +0200

    Fixed image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
             {isClient ? (
                 <div className="ctp-mocha relative h-screen bg-ctp-mantle">
                     <Navbar />
-                    <Image src='https://r2.e-z.host/7ed0180f-b228-49a7-be1e-0183c1938777/06uh5y7g.jpg' width={1920} height={1080} className='absolute inset-0 w-full h-full object-cover z-0 pointer-events-none blur-[5px]' draggable={false} alt='bg' quality={100}/>
+                    <Image src='https://r2.e-z.host/7ed0180f-b228-49a7-be1e-0183c1938777/06uh5y7g.jpg' width={1920} height={1080} className='fixed inset-0 w-full h-full object-cover z-0 pointer-events-none blur-[5px]' draggable={false} alt='bg' quality={100}/>
                     <div className="absolute inset-0 bg-[#181825] bg-opacity-80"></div>
                     <div className="flex flex-col items-center justify-center absolute top-0 left-0 w-full h-full z-10">
                         <div className="bg-ctp-crust bg-opacity-90 py-5 px-10 w-[85%] rounded-xl" id="bar">
