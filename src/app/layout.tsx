abe0ea3bd898a25d55568743c212c881d90ff7d1
zvbt <cyadine@skiff.com>
2024-04-18 13:15:33 +0200

    Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="/favicon.png" type="image/x-png" />
       <body className={karla.className}>{children}</body>
     </html>
   );
