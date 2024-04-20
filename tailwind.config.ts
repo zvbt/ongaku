24e7a2ba56a6a9ab587f47e1435f7c04d429cb61
zvbt <cyadine@skiff.com>
2024-04-21 01:44:24 +0200

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,8 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       screens: {
-        lg: "900px"
+        lg: "900px",
+        ss: "300px"
       }
     },
   },
